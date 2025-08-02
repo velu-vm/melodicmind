d38d11dad4bd20d2271cc0278f2aff4235a26bba
velu-vm <velmuruganyt1412@gmail.com>
2025-08-02 17:31:46 +0530

    Update frontend.tf

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -21,6 +21,15 @@ resource "aws_s3_bucket_website_configuration" "frontend" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "frontend_block" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "public_read" {
   bucket = aws_s3_bucket.frontend.id
 
